5620b92c677cd6d923afc45bfc1f928a90303f2c
soporte2 <soporte2@SISTEMAS1.eduparquesperu.local>
2016-12-22 15:04:26 -0500

    #1->Inicio del proyecto

---
+++ src/java/pe/web/app/bd/AccesoMSSQL.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.web.app.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author soporte2
+ */
+public class AccesoMSSQL {
+    
+     private String USERNAME="sa";
+        private String PASSWORD="diversion";
+        //private String HOST="localhost";
+        //private String PORT="3306";
+       // private String DATABASE="comercio";
+        private String CLASSNAME="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        private String URL="jdbc:sqlserver://192.168.1.74:1433;databaseName=BDTemporal";
+        
+        private Connection con;
+        
+        public AccesoMSSQL(){
+            
+            try {
+                Class.forName(CLASSNAME);
+                con=DriverManager.getConnection(URL,USERNAME,PASSWORD);
+                
+                
+            } catch (ClassNotFoundException e) {  
+                System.err.println(e.getMessage());
+            }catch (SQLException esq) {  
+                System.err.println(esq.getMessage());
+            }
+        
+        }
+        
+        
+        public Connection getConexion(){
+        
+        return con;
+        }
+        
+       //prueba de conexion
+        
+       public static void main(String[] args) {
+        AccesoMSSQL con=new AccesoMSSQL();
+    }
+        
+    
+}
